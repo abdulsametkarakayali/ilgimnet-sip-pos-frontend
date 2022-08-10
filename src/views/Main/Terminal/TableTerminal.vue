184b9f4647e3d9c15c568041f79548f7dfcfc16a
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-08-10 22:27:14 +0300

    getir herşeyi

--- src/views/Main/Terminal/TableTerminal.vue
+++ src/views/Main/Terminal/TableTerminal.vue
@@ -16,21 +16,16 @@
       <template
         v-slot:cell(no)="data"
       >{{currentPage === 1 ? (data.index + 1) : (perPage * (currentPage - 1) + 1) + data.index}}</template>
-      <template v-slot:cell(option)="data">
-        <b-button
-          v-b-modal.modal-primary
-          @click="updateModalData(data.item)"
-          variant="success"
-          class="mr-2 my-1"
-          size="sm"
-        >Update</b-button>
-      </template>
+       <template v-slot:cell(invoice)="data">#{{data.item.descriptions}}</template>
+       <template v-slot:cell(casetime)="data">#{{data.item.casetime}}</template>
+       <template v-slot:cell(moneycaseamount)="data">{{data.item.moneycaseamount}}</template>
+      <template v-slot:cell(casetype)="data">{{data.item.casetype == 1?'Kasa Açıldı': data.item.casetype == 2?'Para Eklendi':data.item.casetype == 3?'Para Çıkarıldı':'Kasa Kapatıldı' }}</template>
     </b-table>
       </div>
 
     <b-pagination
       v-model="currentPage"
-      :total-rows="totalRows === 0 ? moneyCases.length : totalRows"
+      :total-rows="totalRows === 0 ? moneycases.length : totalRows"
       :per-page="perPage"
       first-number
       last-number
@@ -42,16 +37,15 @@
 <script>
 import mixins from '@/components/mixins/swal'
 import { mapActions, mapGetters } from 'vuex'
-
 export default {
   props: ['moneycases'],
   mixins: [mixins],
   data() {
     return {
-      fields: ['no', 'descriptions', 'option'],
+      fields: ['no', 'casetype', 'moneycaseamount', 'descriptions', 'casetime'],
       currentPage: 1,
       perPage: 10,
-      totalRows: this.moneyCases.length
+      totalRows: this.moneycases.length
     }
   },
   methods: {
