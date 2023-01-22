5a3dbc2b04171e472178d4953d78cd92b2880f79
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2023-01-22 17:02:31 +0300

    first commit

--- src/views/Landing/components/Header.vue
+++ src/views/Landing/components/Header.vue
@@ -13,19 +13,7 @@
               class="btn shadow btn-header btn-info mt-2 px-5 ml-3"
             >
               Sisteme Giriş
-              <svg
-                width="30"
-                height="30"
-                viewBox="0 0 16 16"
-                class="bi bi-play"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"
-                />
-              </svg>
+
             </router-link>
           </div>
           <div class="col-md-5 col-sm-12 hide-mobile">
