5519ba6871f6e14e0c868cbffe4390a874d19c1c
abdulsametkarakayali <samedkarakayali@gmail.com>
2023-04-15 12:00:59 +0300

    test make

--- src/store/modules/history.js
+++ src/store/modules/history.js
@@ -267,6 +267,7 @@ const mutations = {
     }
     state.MonthIncome = newHistories.filter(history => history.month === new Date().getMonth()).map(val => Number(val.amount)).reduce((a, b) => a + b,0)
     state.ordersToday = newHistories.filter(history => history.date === todayGlobal && (history.paymentType === 0 || history.paymentType === 1)).length
+    
     state.SafePayment = (newHistories.filter(history => history.date === todayGlobal && history.paymentType !== 1).map(val => Number(val.paymentType === 11 ? -val.amount : val.amount)).reduce((a, b) => a + b))
     state.CardPayment = (newHistories.filter(history => history.paymentType === 1 && history.date === todayGlobal).map(val => Number(val.amount)).reduce((a, b) => a + b))
     state.CashPayment = ( newHistories.filter(history => history.paymentType === 0 && history.date === todayGlobal).map(val => Number(val.amount)).reduce((a, b) => a + b))
